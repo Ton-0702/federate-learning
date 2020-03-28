b37c2cfec855af095926a7fed7b58fae3cb03f9b
Trung H. Le <trustingsocial@Trung-Le-MacBook-Pro.local>
2020-03-29 04:15:05 +0700

    second commit

--- flearn/fed_avg.py
+++ flearn/fed_avg.py
@@ -51,47 +51,6 @@ class Client:
         return self.n, get_grad(model)
 
 
-def client(data,
-           model,
-           opt,
-           loss_func,
-           configs={}
-           ):
-    """
-    # data: dictionary containing 'x' matrix and 'y' vector
-    # model: the model
-    # opt: optimizer
-    # loss_func: loss functions
-    # configs: dictionary containing training configs
-    """
-    x = data['x']
-    y = data['y']
-    n = len(x)
-    n_epochs = configs.get('n_epochs', 5)
-    batch_size = configs.get('batch_size', 8)
-
-    for e in range(n_epochs):
-        # Randomly order training data
-        indices = np.arange(n)
-        random.shuffle(indices)
-        x = x[indices]
-        y = y[indices]
-
-        for i in range(n // batch_size):
-            # Slice the data
-            xs = x[i: i + batch_size]
-            ys = y[i: i + batch_size]
-            # Start training on current batch
-            model.train()
-            opt.zero_grad()
-            ys_bar = model(xs)
-            loss = loss_func(ys, ys_bar)
-            loss.backward()
-            opt.step()
-    # Return client number of samples and gradient
-    return n, get_grad(model)
-
-
 def server(base_model,
            base_opt,
            loss_func,
@@ -105,7 +64,6 @@ def server(base_model,
     for clt in clients:
         n += clt.get_n_samples()
     for r in range(num_rounds):
-        # First iter, get number of samples from all clients
         for clt in clients:
             nk, grad = clt.local_update(model, opt, loss_func)
             for key in grad.keys():
