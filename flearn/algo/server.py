64b553f806162e7a51804c226bbf57e3f3fee439
Giang Tran <tdgiangjvl@gmail.com>
2020-08-02 02:40:23 +0000

    fix qFed

--- flearn/algo/server.py
+++ flearn/algo/server.py
@@ -58,6 +58,15 @@ class BaseServer:
             # print('Train accuracy: ', train_acc)
             # print('Test accuracy: ', test_acc)
             self.metrics.update(-1, clt.name, train_acc, test_acc, None)
+    def my_mod_evaluate(self,r):
+        for clt in self.clients:
+            clt.set_weights(self.model.state_dict())
+            train_acc = clt.get_train_accuracy()
+            test_acc = clt.get_test_accuracy()
+            # print('Name: ', clt.name)
+            # print('Train accuracy: ', train_acc)
+            # print('Test accuracy: ', test_acc)
+            self.metrics.update(-r, clt.name, train_acc, test_acc, None)
 
     def train(self):
         pass
@@ -72,8 +81,9 @@ class BaseServer:
         nks = self.get_nks()
         return np.random.choice(
             self.clients,
-            len(self.clients),
-            p=[e/sum(nks) for e in nks]
+            int(len(self.clients)*self.pct_client_per_round),
+            p=[e/sum(nks) for e in nks],
+            replace=False
         )
 
 
@@ -163,7 +173,8 @@ class QFedAvgServer(BaseServer):
         simulated_grads = {}
         deltas = {}
         hs = {}
-        for r in range(self.num_rounds):
+        for r in range(1,self.num_rounds+1):
+            print('training round : {}'.format(r))
             for name, param in self.model.named_parameters():
                 simulated_grads[name] = param.clone()
                 deltas[name] = []
@@ -175,7 +186,7 @@ class QFedAvgServer(BaseServer):
                 ws, error, acc = clt.solve_avg(self.num_epochs, self.batch_size)
                 self.metrics.update(r, clt.name, error, acc, None)
                 for key in ws.keys():
-                    simulated_grads[key] -= ws[key]
+                    simulated_grads[key] = self.model.state_dict()[key]-ws[key]
                     simulated_grads[key] *= 1.0/self.lr
                     deltas[key].append(np.float_power(error + 1e-10, self.q) * simulated_grads[key])
                     hs[key].append(self.q * np.float_power(error + 1e-10, (self.q - 1)) *
@@ -185,6 +196,44 @@ class QFedAvgServer(BaseServer):
                 total_delta = functools.reduce(operator.add, deltas[key])
                 total_h = functools.reduce(operator.add, hs[key])
                 self.model.state_dict()[key] -= total_delta / total_h
+            self.my_mod_evaluate(r)
+
+
+class DL_FedAvgServer(BaseServer):
+    def __init__(self, model, opt, lossf, clients, train_data, test_data,
+                 dataset_name, method_name, configs=None):
+        super().__init__(model, opt, lossf, clients, train_data, test_data,
+                         dataset_name, method_name, configs)
+    def DL_get_nks(self):
+        return [c.get_num_samples()*c.get_lambda()+1e-20 for c in self.clients]
+
+    def DL_sample_clients(self,):
+        nks = self.DL_get_nks()
+        return np.random.choice(
+            self.clients,
+            int(len(self.clients)*self.pct_client_per_round),
+            p=[e/sum(nks) for e in nks],
+            replace=False
+        )
+
+    def train(self,):
+        for r in range(self.num_rounds):
+            n = 0
+            Ls = []
+            sub_clients = self.DL_sample_clients()
+            for clt in sub_clients:
+                n += clt.get_num_samples()*clt.get_lambda()+1e-20
+            for clt in sub_clients:
+                clt.set_weights(self.model.state_dict())
+                ws, error, acc = clt.solve_avg(self.num_epochs, self.batch_size)
+                Ls.append(error)
+                self.metrics.update(r, clt.name, error, acc, None)
+                for key in ws.keys():
+                    self.model.state_dict()[key] += ((clt.get_num_samples()*clt.get_lambda()+1e-20) / n) * ws[key]
+            for clt,Li in zip(sub_clients,Ls):
+                clt.update_lambda(clt.get_lambda()+self.s*np.abs(Li-sum(Ls)/len(Ls)))
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flearn.algo.client import Client
-from flearn.algo.server import FedAvgServer, FedSgdServer, QFedSgdServer, QFedAvgServer
+from flearn.algo.client import Client, DL_Client
+from flearn.algo.server import FedAvgServer, FedSgdServer, QFedSgdServer, QFedAvgServer, DL_FedAvgServer
 from flearn.model.mlp import MLP
 from flearn.utils import read_data
 import torch
@@ -9,7 +9,9 @@ import torch.nn as nn
 
 def run_app(train_dir,
             test_dir,
-            configs
+            configs,
+            server_configs=None,
+            return_flag=False,
             ):
     client_names, groups, train_data, test_data = read_data(train_dir, test_dir, torch.long)
 
@@ -20,23 +22,36 @@ def run_app(train_dir,
 
     base_model = MLP(layer_sizes, act_funcs)
     base_opt = optim.SGD(params=base_model.parameters(), lr=0.05)
-
-    clients = []
-    for c_name in client_names:
-        clients.append(Client(c_name, [], train_data[c_name], test_data[c_name], base_model, base_opt, lossf))
-
-    server = FedAvgServer(model=base_model,
+    if 'DL_' in configs['method_name']:
+        clients = []
+        for c_name in client_names:
+            clients.append(DL_Client(c_name, [], train_data[c_name], test_data[c_name], base_model, base_opt, lossf))
+    else:
+        clients = []
+        for c_name in client_names:
+            clients.append(Client(c_name, [], train_data[c_name], test_data[c_name], base_model, base_opt, lossf))
+    switch={
+      'FedAvgServer':FedAvgServer,
+      'FedSgdServer':FedSgdServer,
+      'QFedSgdServer':QFedSgdServer,
+      'QFedAvgServer':QFedAvgServer,
+      'DL_FedAvgServer':DL_FedAvgServer,
+    }
+    server = switch[configs['method_name']](model=base_model,
                           opt=base_opt,
                           lossf=lossf,
                           clients=clients,
                           train_data=train_data,
                           test_data=test_data,
                           dataset_name=configs['dataset_name'],
-                          method_name=configs['method_name']
+                          method_name=configs['method_name'],
+                          configs=server_configs,
                           )
     server.train()
-    server.evaluate()
+    # server.evaluate()
     server.report()
+    if return_flag:
+      return server
 
 
 if __name__ == '__main__':
